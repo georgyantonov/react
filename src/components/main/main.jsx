c0af2e90a35e49d8f84935b172b8edd62d40e08a
Georgiy Antonov <georgy.antonov@nova-test.ru>
2022-06-12 16:14:38 +0300

    добавил мейн и часть первого скрина

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -1,7 +1,23 @@
 import React from 'react'
+import image1 from "./Group 14.png"
+import './style.css'
 
 export default function Main() {
   return (
-    <div>Main</div>
+    <main>
+      <div className='first_screen'>
+          <div className='start'>
+            <h1>Take care of yours family's <span>health.</span></h1>
+            <h2>All in one destination for COVID-19 health queries.
+              <br></br>Consult 10,000+ health workers about your concerns.</h2>
+            <button>
+              <p>Get started</p>
+            </button>
+          </div>
+          <div className='start_img'>
+            <img src={image1} alt="Doctor" />
+          </div>
+      </div>
+    </main>
   )
 }
