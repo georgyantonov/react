d1e4e38411eec48abb984a989d1f30f6bb23774c
Georgiy Antonov <georgy.antonov@nova-test.ru>
2022-07-12 23:22:23 +0300

    сверстал поп-ап

--- src/components/header/button/button.jsx
+++ src/components/header/button/button.jsx
@@ -3,11 +3,21 @@ import React from 'react'
 
 
 export default function Button() {
+  
   return (
     <div className='button_container'>
         <button className='download'>
             <p>Download</p>
         </button>
+        <div className="download_wrapper">
+          <div className="close"></div>
+          <div className="download_wrapper_content">
+            <h1>Download our App</h1>
+            <button className='download'>
+              <p>Download</p>
+            </button>
+          </div>
+        </div>
     </div>
   )
 }
