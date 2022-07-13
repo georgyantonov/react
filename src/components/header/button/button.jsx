92881eee552d143198c8acb3577d195afdfaa3f7
georgyantonov <georgy.antonov@dalee.ru>
2022-07-13 16:54:38 +0300

    сделал попап на десктопе

--- src/components/header/button/button.jsx
+++ src/components/header/button/button.jsx
@@ -3,21 +3,31 @@ import React from 'react'
 
 
 export default function Button() {
-  
+  function openPopUp(e){
+    console.dir(e)
+    let popup = document.querySelector('.download_wrapper')
+    e.preventDefault();
+    popup.classList.add('active')
+  }
+  function closePopUp (e){
+    e.stopPropagation()
+    let popup = document.querySelector('.active')
+    popup.classList.remove('active')
+  }
   return (
-    <div className='button_container'>
-        <button className='download'>
+    <div className='button_container'onClick={(e)=>openPopUp(e)}>
+        <button className='download' >
             <p>Download</p>
         </button>
         <div className="download_wrapper">
-          <div className="close"></div>
-          <div className="download_wrapper_content">
-            <h1>Download our App</h1>
-            <button className='download'>
-              <p>Download</p>
-            </button>
-          </div>
-        </div>
+              <div className="download_wrapper_content">
+                <div className="close"onClick={(e)=>closePopUp(e)}></div>
+                <h1>Download our App</h1>
+                <button className='download'>
+                  <p>Download</p>
+                </button>
+              </div>
+            </div>
     </div>
   )
 }
