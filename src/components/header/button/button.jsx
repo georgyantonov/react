fb5a6d7b46b4270b00ee0ee355f6d606dbda1b82
georgyantonov <georgy.antonov@dalee.ru>
2022-07-13 17:57:48 +0300

    фиксы попапа

--- src/components/header/button/button.jsx
+++ src/components/header/button/button.jsx
@@ -4,6 +4,7 @@ import React from 'react'
 
 export default function Button() {
   function openPopUp(e){
+    e.stopPropagation()
     console.dir(e)
     let popup = document.querySelector('.download_wrapper')
     e.preventDefault();
